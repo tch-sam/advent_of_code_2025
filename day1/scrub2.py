79b77ca1ef39f1c1814aeb2afd0fb5a749c8cb47
tch-sam <tchsam@utexas.edu>
2025-12-06 03:47:44 +0000

    Setting up day 2

---
+++ day1/scrub2.py
@@ -0,0 +1,22 @@
+file_path = 'day1/day1_input.txt'
+
+with open(file_path, 'r') as file:
+    # Use a list comprehension to read each line, strip whitespace/newlines,
+    # and store the clean string in a list.
+    my_inputs = [line.strip() for line in file]
+
+
+current = 50
+zero_passed_counter = 0
+
+for turn in my_inputs:
+    direction = turn[0]
+    mag = int(turn[1:])
+
+    if direction == 'L':
+        current = (current - mag) % 100
+    else:
+        current = (current + mag) % 100
+
+    if current == 0:
+        zero_counter += 1
